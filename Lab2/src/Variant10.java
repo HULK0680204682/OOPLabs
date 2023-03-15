eaf92321b016581e50f185ee2a574b5f5ae98645
HULK0680204682 <rijs4407@gmail.com>
2023-03-15 18:56:08 +0200

    lab2 done

--- Lab2/src/Variant10.java
+++ Lab2/src/Variant10.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Variant10 {
+public class Variant10 {
+    public static void main(String[] args) {
+        int wordCount = 0;
+        StringBuffer text = new StringBuffer("Ввести текст, що_містить_пару спеціальних символів, які ( вводяться наприклад, дужки ) та або зірочки");
+        int initialDeleteIndex = text.indexOf("(");
+        int finalDeletionIndex = text.indexOf(")");
+        int finalDeletionIndexPlus = finalDeletionIndex + 1;
+        System.out.print(text);
+        text.delete(initialDeleteIndex, finalDeletionIndexPlus);
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == ' ' | text.charAt(i) == '_') {
+                wordCount++;
+            }
+        }
+        System.out.print("\n" + text);
+        System.out.print("\n" + wordCount);
+    }
 }
