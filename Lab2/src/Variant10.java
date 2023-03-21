a65d03ef338d044ee0b86e451b3ef2be46344138
HULK0680204682 <rijs4407@gmail.com>
2023-03-21 15:14:03 +0200

    add some code

--- Lab2/src/Variant10.java
+++ Lab2/src/Variant10.java
@@ -1,18 +1,14 @@
-public class Variant10 {
-    public static void main(String[] args) {
-        int wordCount = 0;
-        StringBuffer text = new StringBuffer("Ввести текст, що_містить_пару спеціальних символів, які ( вводяться наприклад, дужки ) та або зірочки");
-        int initialDeleteIndex = text.indexOf("(");
-        int finalDeletionIndex = text.indexOf(")");
-        int finalDeletionIndexPlus = finalDeletionIndex + 1;
-        System.out.print(text);
-        text.delete(initialDeleteIndex, finalDeletionIndexPlus);
-        for (int i = 0; i < text.length(); i++) {
-            if (text.charAt(i) == ' ' | text.charAt(i) == '_') {
-                wordCount++;
-            }
-        }
-        System.out.print("\n" + text);
-        System.out.print("\n" + wordCount);
-    }
+public  class Variant10 {
+   public static void removal()
+   {
+       Main.endIndex++;
+        Main.text.delete(Main.initialIndex, Main.endIndex);
+       for (int i = 0; i < Main.text.length(); i++) {
+           if (Main.text.charAt(i) == ' ' | Main.text.charAt(i) == '_') {
+               Main.wordCount++;
+           }
+       }
+       Main.output();
+       System.out.print("\n" + Main.wordCount);
+   }
 }
