9c62192b25e2ffb4521b7b03a52c6232f33a78a4
RuslanSlobodian <ruslanslobodian87@gmail.com>
2023-04-13 13:44:53 +0300

    done lab2

--- Lab2/src/Main.java
+++ Lab2/src/Main.java
@@ -1,16 +1,9 @@
-
 public class Main {
-    public static StringBuffer text = new StringBuffer("Ввести текст, що_містить_пару спеціальних символів, які ( вводяться наприклад, дужки ) та або зірочки");
-    public static int wordCount = 0;;
-    public static int initialIndex = text.indexOf("(");
-    public static  int endIndex = text.indexOf(")");
-
     public static void main(String[] args) {
-        Main.output();
-       Variant10.removal();
-    }
-    public static void output() {
-        System.out.print("\n" + text);
+        Variant10 variant10 = new Variant10("Ввести текст, що_містить_пару спеціальних символів, які (вводяться наприклад, дужки) та або зірочки");
 
+        System.out.println("Вихідний текст:\n" + variant10.getInitialText());
+        System.out.println("Кількість слів: " + variant10.getWordCount());
+        System.out.println("Змінений текст:\n" + variant10.getChangedText());
     }
 }
--- Lab2/src/Variant10.java
+++ Lab2/src/Variant10.java
@@ -1,14 +1,41 @@
-public  class Variant10 {
-   public static void removal()
-   {
-       Main.endIndex++;
-        Main.text.delete(Main.initialIndex, Main.endIndex);
-       for (int i = 0; i < Main.text.length(); i++) {
-           if (Main.text.charAt(i) == ' ' | Main.text.charAt(i) == '_') {
-               Main.wordCount++;
-           }
-       }
-       Main.output();
-       System.out.print("\n" + Main.wordCount);
-   }
+public class Variant10 {
+    private int wordCount;
+
+    private final String initialText;
+    private final String changedText;
+
+    public String getChangedText() {
+        return changedText;
+    }
+
+    public int getWordCount() {
+        return wordCount;
+    }
+
+    public String getInitialText() {
+        return initialText;
+    }
+
+    public Variant10(String text) {
+        initialText = text;
+        wordCount = Variant10.countWords(text);  //правильний виклик статичного методу(через назву класу)
+        changedText = deleteTextInBrackets(text);
+    }
+
+    private String deleteTextInBrackets(String text) {
+        StringBuilder builder = new StringBuilder(text);
+        int initialIndex;
+        while ((initialIndex = builder.indexOf("(")) != -1) {
+            int endIndex = builder.indexOf(")");
+            if (endIndex == -1) {
+                endIndex = builder.length();
+            }
+            builder.delete(initialIndex, endIndex+1);
+        }
+        return builder.toString();
+    }
+
+    public static int countWords(String text) {
+        return text.split("[\\s_]").length;
+    }
 }
