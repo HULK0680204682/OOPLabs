e25ebe45b5ccabcfe44054fc19ebed7f97f8a6af
HULK0680204682 <rijs4407@gmail.com>
2023-04-20 14:15:09 +0300

    “lab2.2”

--- Lab2/src/Main.java
+++ Lab2/src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static StringBuffer text = new StringBuffer("Ввести текст, що_містить_пару спеціальних символів, які ( вводяться наприклад, дужки ) та або зірочки");
-    public static int wordCount = 0;;
+    public static int wordCount = 0;
     public static int initialIndex = text.indexOf("(");
     public static  int endIndex = text.indexOf(")");
 
